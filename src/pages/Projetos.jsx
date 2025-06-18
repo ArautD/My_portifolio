e9c92c0bb828a1109a5ce0904b8c9d888d53bd66
ARAUT <sergiosg6@yahoo.com>
2025-06-18 18:36:23 -0300

    Ignorando arquivo

---
+++ src/pages/Projetos.jsx
@@ -0,0 +1,10 @@
+function Projetos() {
+  return (
+    <div>
+      <h1>Bem-vindo à Home</h1>
+      <p>Esse é meu portfólio!</p>
+    </div>
+  )
+}
+
+export default Projetos
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,10 @@
+function Sobre() {
+  return (
+    <div>
+      <h1>Bem-vindo à Home</h1>
+      <p>Esse é meu portfólio!</p>
+    </div>
+  )
+}
+
+export default Sobre
